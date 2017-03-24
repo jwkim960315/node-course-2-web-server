9bd6d191319e5cf8bb86a1d34358568a0f8b9760
jwkim960315 <jkim11@wesleyan.edu>
2017-03-24 17:24:51 +0900

    Edited Port to port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.Port || 3000;
+const port = process.env.port || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
